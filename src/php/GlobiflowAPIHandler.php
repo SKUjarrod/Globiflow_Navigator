ce99b5f54e2c7fa43842a57e7c94e9ef999d7ff9
SKUjarrod <jarrod.adair@skuvantage.com>
2022-05-17 16:17:27 +1000

    Added debugging and worked on session manager with other php code

--- src/php/GlobiflowAPIHandler.php
+++ src/php/GlobiflowAPIHandler.php
@@ -8,17 +8,17 @@ $client_secret = "yn4Lg5ytBtPO8HRu5irHoP3kb7HLmwmBLMEhhpgUmfcKgFLCMKe5OFXC6lHlRt
 $app_id = "27204671";
 $app_token = "c1f9def98286b1ed52871b80c4e2711b";
 
-// might not be needed with server-sided authentication below
 ///////////////////////////////////////
 // start server-sided authentication //
 ///////////////////////////////////////
-Podio::setup($client_id, $client_secret);
 
 // Set up the REDIRECT_URI -- which is just the URL for this file.
-define("REDIRECT_URI", 'http://localhost:8000/GlobiflowAPIHandler.php');
+define("REDIRECT_URI", 'http://localhost:80');
 // Set up the scope string
 define("SCOPE", 'user:read space:all');
-Podio::setup($client_id, $client_secret);
+Podio::setup($client_id, $client_secret, array(
+  "session_manager" => "PodioBrowserSession"
+));
 
 if (!isset($_GET['code']) && !Podio::is_authenticated()) {
 
@@ -47,6 +47,7 @@ elseif (isset($_GET['code'])) {
     // Finalize authentication. Note that we must pass the REDIRECT_URI again.
     Podio::authenticate_with_authorization_code($_GET['code'], REDIRECT_URI);
     print "You have been authenticated. Wee!";
+
   }
 
 }
@@ -55,12 +56,12 @@ elseif (isset($_GET['code'])) {
 //  end server-sided authentication  //
 ///////////////////////////////////////
 
+if (Podio::is_authenticated()) {
+  print_r(
+    // PodioOrganization::get_all()
+    PodioSpace::get_available("139615")
+    // PodioApp::get_for_space($app_id, $app_token);
 
-
-// Podio::authenticate_with_app($app_id, $app_token);
-// $items = PodioItem::filter($app_id);
-
-// print "My app has ".count($items)." items";
-
-
+    );
+}
 ?>
